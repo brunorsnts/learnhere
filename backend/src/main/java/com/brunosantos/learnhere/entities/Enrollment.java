1b33826e9dd9e512762b937efa87cd8d2534aca8
brunorsnts <santos_2002@outlook.com.br>
2026-01-27 19:30:15 -0300

    Enrollment, EnrollmentPK

---
+++ backend/src/main/java/com/brunosantos/learnhere/entities/Enrollment.java
@@ -0,0 +1,49 @@
+package com.brunosantos.learnhere.entities;
+
+import com.brunosantos.learnhere.entities.pk.EnrollmentPK;
+import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_enrollment")
+public class Enrollment {
+
+    @Getter(AccessLevel.NONE)
+    @Setter(AccessLevel.NONE)
+    @EmbeddedId
+    private EnrollmentPK id = new EnrollmentPK();
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant enrollMoment;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant refundMoment;
+
+    private boolean available;
+    private boolean onlyUpdate;
+
+    public User getStudent() {
+        return id.getUser();
+    }
+
+    public void setStudent(User user) {
+        id.setUser(user);
+    }
+
+    public Offer getOffer() {
+        return id.getOffer();
+    }
+
+    public void setOffer(Offer offer) {
+        id.setOffer(offer);
+    }
+}
---
+++ backend/src/main/java/com/brunosantos/learnhere/entities/pk/EnrollmentPK.java
@@ -0,0 +1,25 @@
+package com.brunosantos.learnhere.entities.pk;
+
+import com.brunosantos.learnhere.entities.Offer;
+import com.brunosantos.learnhere.entities.User;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Embeddable
+public class EnrollmentPK {
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "offer_id")
+    private Offer offer;
+}
