e668c1a84efa8a37433b2a9dcacfffae8b1da1ed
brunorsnts <santos_2002@outlook.com.br>
2026-01-28 07:41:29 -0300

    Lesson, Content and Task

---
+++ backend/src/main/java/com/brunosantos/learnhere/entities/Lesson.java
@@ -0,0 +1,38 @@
+package com.brunosantos.learnhere.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "tb_lesson")
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class Lesson {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String title;
+    private Integer position;
+
+    @ManyToOne
+    @JoinColumn(name = "section_id")
+    private Section section;
+
+    @Setter(AccessLevel.NONE)
+    @ManyToMany
+    @JoinTable(name = "tb_lessons_done",
+    joinColumns = @JoinColumn(name = "lesson_id"),
+    inverseJoinColumns = {
+            @JoinColumn(name = "user_id"),
+            @JoinColumn(name = "offer_id")
+    })
+    private Set<Enrollment> enrollmentsDone = new HashSet<>();
+}
